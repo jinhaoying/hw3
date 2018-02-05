08370f5b1150d8cd084c8809ea8a4fcaf2095a51
jinhaoying <35727900+jinhaoying@users.noreply.github.com>
2018-02-05 14:13:14 -0800

    Update taller-lines.js

--- taller-lines.js
+++ taller-lines.js
@@ -1,8 +1,9 @@
 stroke(0);
-strokeWeight(2);
+strokeWeight(1);
 
-for (i = 0; i < 70; i ++) {
-  gap = i*5;
-  line(gap, 0, gap, 0+gap);
+for (i = 0; i < 120; i ++) {
+  gap1 = i*3;
+  gap2 = i*10
+  line(gap1, 0, gap1, 0+gap2);
   
 }
