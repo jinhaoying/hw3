eb22807e6580e4aca2d405134513f1060a5337c4
jinhaoying <35727900+jinhaoying@users.noreply.github.com>
2018-02-22 22:14:26 -0800

    Update cone-of-lines.js

--- cone-of-lines.js
+++ cone-of-lines.js
@@ -40,3 +40,15 @@ function draw(){
 	x=mouseX;
   y = mouseY;
 }
+
+------------
+stroke(0);
+strokeWeight(2);
+
+function draw(){
+	for (i = 0; i < 18; i ++) {
+  	gap = 55 + i*15;
+  	line(mouseX, mouseY, gap, 320)
+	}
+  clear();
+}
