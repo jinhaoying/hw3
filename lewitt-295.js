96d710929c2fb2dea529d811194d3f4a1e20e9d2
jinhaoying <35727900+jinhaoying@users.noreply.github.com>
2018-02-22 21:48:12 -0800

    Create lewitt-295.js

---
+++ lewitt-295.js
@@ -0,0 +1,22 @@
+background(0)
+fill(255);
+rect(45,45,250,250)
+fill(0);
+rect(48,48,244,244)
+fill(255);
+ellipse(170, 170, 250, 250);
+fill(0);
+ellipse(170, 170, 245, 245);
+stroke(255);
+
+//midslants
+line(46,295,170,45);
+line(170,45,294,295);
+
+//quarterslants
+line(46,295,108,45);
+line(232,45,294,295);
+
+//quarterlines
+line(108,45,108,295);
+line(232,45,232,295);
