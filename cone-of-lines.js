4dda6afe64fa44d1f9390e5a4f1a28481d14f4c8
jinhaoying <35727900+jinhaoying@users.noreply.github.com>
2018-02-22 22:17:27 -0800

    Update cone-of-lines.js

--- cone-of-lines.js
+++ cone-of-lines.js
@@ -52,3 +52,32 @@ function draw(){
 	}
   clear();
 }
+
+------------
+	
+stroke(0);
+strokeWeight(2);
+
+function draw(){
+  	line(mouseX, mouseY, 55, 320)
+    line(mouseX, mouseY, 70, 320)
+    line(mouseX, mouseY, 85, 320)
+    line(mouseX, mouseY, 100, 320)
+    line(mouseX, mouseY, 115, 320)
+    
+    line(mouseX, mouseY, 130, 320)
+    line(mouseX, mouseY, 145, 320)
+    line(mouseX, mouseY, 160, 320)
+    line(mouseX, mouseY, 175, 320)
+    line(mouseX, mouseY, 190, 320)
+    
+    line(mouseX, mouseY, 205, 320)
+    line(mouseX, mouseY, 220, 320)
+    line(mouseX, mouseY, 235, 320)
+    line(mouseX, mouseY, 250, 320)
+    line(mouseX, mouseY, 265, 320)
+    
+    line(mouseX, mouseY, 280, 320)
+    line(mouseX, mouseY, 295, 320)
+    line(mouseX, mouseY, 310, 320)
+}
